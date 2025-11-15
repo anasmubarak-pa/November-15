a97474f4df0800ba5ce48fcf361c69d0a78584f2
Anas-Mubarak <anasmubarak435@gmail.com>
2025-11-15 12:48:33 +0530

    morning

--- validate-email-task-7/validate-email.ts
+++ validate-email-task-7/validate-email.ts
@@ -19,7 +19,7 @@ function validateMail(mailId:string)
     if(!validation(header,['.','_','-']))return false
     if(footer.split('.').length!=2)return false
     let [domain,tld] = footer.split('.')
-    if(!validation(domain,[''])||tld.length<2||!validation(tld,[])) return false
+    if(!validation(domain,['-'])||tld.length<2||!validation(tld,[])||domain[0]=='-'||domain[domain.length-1]=='-') return false
     return true
 }
 
@@ -32,3 +32,15 @@ console.log(returnValid(["test@mail.com", "abc@", "ok@gmail.com"]))
 
 // console.log(validateMail('@gmail.com'))
 // console.log(validateMail('he2l.lo@gmail.com'))
+function testOuput(recieved:Array<string>,expected:Array<string>)
+{
+    JSON.stringify(recieved)===JSON.stringify(expected)?console.log('Test Pass'):console.log('test fail')
+}
+
+testOuput(returnValid(["test@mail.com", "abc@", "ok@gmail.com"]),['test@mail.com',"ok@gmail.com"])
+testOuput(returnValid(["test@mail.com", "abc@",'.bcd@gmail.com', "ok@gmail.com"]),['test@mail.com',"ok@gmail.com"])
+testOuput(returnValid(["test@mail.com", "abc@",'.bcd@gmail.com', "ok@gmail.com"]),['test@mail.com',"ok@gmail.com"])
+testOuput(returnValid(["test@mail.com", "abc@", "ok_7@gmail.com"]),['test@mail.com',"ok_7@gmail.com"])
+testOuput(returnValid(["test@mail.com", "abc@", "ok_7@gmai_l.com"]),['test@mail.com'])
+testOuput(returnValid(["test@e-mail.com", "abc@", "ok_7@gmai_l.com"]),['test@e-mail.com'])
+testOuput(returnValid(["test@-email.com", "abc@", "ok_7@gmai_l.com"]),[])
